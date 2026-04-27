4dd97079b4fa1be733a94e3fdeddcd87214167ca
juanes <juanesteban10201@gmail.com>
2026-04-27 17:16:51 -0300

    Small changes to water levels, changed some ui, reworked all dialogues to boss, changed most npc dialogues, and changed internally the dialogue system

--- assets/Nodes/Obj/Ui/button.gd
+++ assets/Nodes/Obj/Ui/button.gd
@@ -3,5 +3,5 @@ extends Button
 @export var Scene : String = "select_level_bside"
 
 func _on_pressed() -> void:
-	var _scene_string : String = "res://assets/Levels/world1/" + Scene + ".tscn"
+	var _scene_string : String = Scene
 	get_tree().change_scene_to_file(_scene_string)
