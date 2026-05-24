f623be4113d9b0df91b3a899112a4dd6f56f4009
juanes <juanesteban10201@gmail.com>
2026-05-23 22:54:49 -0300

    mainly wind charge code changes

--- assets/Nodes/Levels/world_test/air_railed_nodes.gd
+++ assets/Nodes/Levels/world_test/air_railed_nodes.gd
@@ -1,10 +1,13 @@
 extends Node2D
 @export var Positions : Array[Vector2]
+@export var Progress : Array[float] = []
 
 func _ready() -> void:
 	for i in range(get_children().size()) :
 		var Child = get_child(i)
-		if(Child): Positions[i] = Child.global_position
+		if(Child):
+			Positions.append(Child.global_position)
+			#print(Path.curve.get_closest_offset(to_local(Child.global_position)))
 
 func get_position_child_index(Index : int) -> Vector2:
 	return Positions[Index]
--- assets/Nodes/Levels/world_test/wind_charge.gd
+++ assets/Nodes/Levels/world_test/wind_charge.gd
@@ -1,3 +1,4 @@
+@tool
 extends Line2D
 
 @export var MoveRef : Node2D
@@ -8,15 +9,19 @@ extends Line2D
 @export var EndWithSlam : bool = false
 @export var RailedNodes : Node
 
-
-func _ready() -> void:
+func _reload() -> void:
 	Path.global_position = self.global_position
 	Path.curve.clear_points()
 	for point in points:
 		Path.curve.add_point(point)
 
+func _ready() -> void:
+	_reload()
+
 func _process(delta: float) -> void:
-	for RailedNode in RailedNodes.get_children():
-		RailedNode.global_position = MoveRef.global_position + RailedNodes.get_position_child(RailedNode)
-		if(RailedNode.Sprite):
-			RailedNode.Sprite.rotation_degrees = PathFollow.rotation_degrees
+	if Engine.is_editor_hint():
+		for i in range(points.size()):
+			var pos : Vector2 = Path.curve.get_point_position(i)
+			if !pos or pos not in points:
+				print("Reloading Wind Sand")
+				_reload()
